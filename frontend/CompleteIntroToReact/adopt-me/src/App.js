6fb943d9019c2df33d290d8e5cf516722384702b
Brenox889 <brenomendes889@hotmail.com>
2020-09-07 23:23:36 -0300

    refact: js react

--- frontend/CompleteIntroToReact/adopt-me/src/App.js
+++ frontend/CompleteIntroToReact/adopt-me/src/App.js
@@ -3,27 +3,36 @@ import ReactDOM from "react-dom";
 import { Pet } from "./Pet";
 
 const App = () => {
-  return React.createElement(
-    "div",
-    { id: "something-important" },
-    React.createElement("h1", {}, "Adopt Me!"),
-    [
-      React.createElement(Pet, {
-        name: "Luna",
-        animal: "Dog",
-        breed: "Havanese",
-      }),
-      React.createElement(Pet, {
-        name: "Pepper",
-        animal: "Bird",
-        breed: "Cockatiel",
-      }),
-      React.createElement(Pet, {
-        name: "Chugar",
-        animal: "Cat",
-        breed: "Mixed",
-      }),
-    ]
+  // return React.createElement(
+  //   "div",
+  //   { id: "something-important" },
+  //   React.createElement("h1", {}, "Adopt Me!"),
+  //   [
+  //     React.createElement(Pet, {
+  //       name: "Luna",
+  //       animal: "Dog",
+  //       breed: "Havanese",
+  //     }),
+  //     React.createElement(Pet, {
+  //       name: "Pepper",
+  //       animal: "Bird",
+  //       breed: "Cockatiel",
+  //     }),
+  //     React.createElement(Pet, {
+  //       name: "Chugar",
+  //       animal: "Cat",
+  //       breed: "Mixed",
+  //     }),
+  //   ]
+  // );
+
+  return (
+    <div>
+      <h1>Adopte Me!</h1>
+      <Pet name="Luna" animal="Dog" breed="Havanese" />
+      <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
+      <Pet name="Chugar" animal="Cat" breed="Mixed" />
+    </div>
   );
 };
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
