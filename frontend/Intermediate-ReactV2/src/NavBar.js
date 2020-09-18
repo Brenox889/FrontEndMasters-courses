aa97a015c109bb831299f299a6539aa14cf52976
Brenox889 <brenomendes889@hotmail.com>
2020-09-18 18:19:26 -0300

    refactor: NavBar

--- frontend/Intermediate-ReactV2/src/NavBar.js
+++ frontend/Intermediate-ReactV2/src/NavBar.js
@@ -1,17 +1,19 @@
 import React from "react";
+
 import { Link } from "@reach/router";
 import { css } from "@emotion/core";
 
-const NavBar = () => {
-  <header
-    css={css`
-      background-color: #333;
-      padding: 15px;
-    `}
-  >
-    <Link to="/">Adopt Me!</Link>
-    <span role="img" aria-label="logo"></span>
-  </header>;
-};
+function NavBar() {
+  return (
+    <header
+      css={css`
+        background-color: #333;
+        padding: 55px;
+      `}
+    >
+      <Link to="/">Adopt Me!</Link>
+    </header>
+  );
+}
 
 export default NavBar;
