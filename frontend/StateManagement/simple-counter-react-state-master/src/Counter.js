1373295808dcb6ad8e4ceba5c0a10244c7f91ede
Brenox889 <brenomendes889@hotmail.com>
2020-09-24 17:15:58 -0300

    feat: customHook useLocalStorage

--- frontend/StateManagement/simple-counter-react-state-master/src/Counter.js
+++ frontend/StateManagement/simple-counter-react-state-master/src/Counter.js
@@ -1,12 +1,28 @@
 import React, { Component, useState, useEffect } from 'react';
 
+const useLocalStorage = (initialState, key) => {
+  const get = () => {
+    const storage = localStorage.getItem(key);
+    if (storage) return JSON.parse(storage)[value];
+    return initialState;
+  };
+  const [value, setValue] = useState(get());
+
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify({ value }));
+  }, [value]);
+  return [value, setValue];
+};
 const Counter = ({ max, step }) => {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useLocalStorage(0);
 
   const increment = () => setCount(count + 1);
   const decrement = () => setCount(count - 1);
   const redefine = () => setCount(0);
 
+  useEffect(() => {
+    document.title = `Counter: ${count}`;
+  }, [count]);
   return (
     <div className="Counter">
       <p className="count">{count}</p>
