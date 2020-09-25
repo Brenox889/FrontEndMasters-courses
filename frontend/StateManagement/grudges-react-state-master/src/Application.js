43363fd764f82fe678d50a8e6398b275542d8362
Brenox889 <brenomendes889@hotmail.com>
2020-09-25 18:17:05 -0300

    feat: reducer function

--- frontend/StateManagement/grudges-react-state-master/src/Application.js
+++ frontend/StateManagement/grudges-react-state-master/src/Application.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useReducer } from 'react';
 
 import id from 'uuid/v4';
 
@@ -7,22 +7,25 @@ import NewGrudge from './NewGrudge';
 
 import initialState from './initialState';
 
+const reducer = (state, action) => {
+  return state;
+};
 const Application = () => {
-  const [grudges, setGrudges] = useState(initialState);
+  const [grudges, dispatch] = useReducer(reducer, initialState);
 
-  const addGrudge = grudge => {
+  const addGrudge = (grudge) => {
     grudge.id = id();
     grudge.forgiven = false;
-    setGrudges([grudge, ...grudges]);
+    // setGrudges([grudge, ...grudges]);
   };
 
-  const toggleForgiveness = id => {
-    setGrudges(
-      grudges.map(grudge => {
-        if (grudge.id !== id) return grudge;
-        return { ...grudge, forgiven: !grudge.forgiven };
-      })
-    );
+  const toggleForgiveness = (id) => {
+    // setGrudges(
+    //   grudges.map((grudge) => {
+    //     if (grudge.id !== id) return grudge;
+    //     return { ...grudge, forgiven: !grudge.forgiven };
+    //   })
+    // );
   };
 
   return (
