e7c364bbb8c1749e9256ea800af2b3eb0fe135e9
Brenox889 <brenomendes889@hotmail.com>
2020-09-26 16:17:47 -0300

    feat: react.memo and useCallback

--- frontend/StateManagement/grudges-react-state-master/src/Application.js
+++ frontend/StateManagement/grudges-react-state-master/src/Application.js
@@ -1,4 +1,4 @@
-import React, { useState, useReducer } from 'react';
+import React, { useState, useReducer, useCallback } from 'react';
 
 import id from 'uuid/v4';
 
@@ -9,6 +9,7 @@ import initialState from './initialState';
 
 const GRUDGE_ADD = 'GRUDGE_ADD';
 const GRUDGE_FORGIVE = 'GRUDGE_FORGIVE';
+
 const reducer = (state, action) => {
   if (action.type === GRUDGE_ADD) {
     return [, action.payload, ...state];
@@ -24,24 +25,30 @@ const reducer = (state, action) => {
 const Application = () => {
   const [grudges, dispatch] = useReducer(reducer, initialState);
 
-  const addGrudge = ({ person, reason }) => {
-    dispatch({
-      type: GRUDGE_ADD,
-      payload: {
-        person,
-        reason,
-        forgiven: false,
-        id: id()
-      }
-    });
-  };
+  const addGrudge = useCallback(
+    ({ person, reason }) => {
+      dispatch({
+        type: GRUDGE_ADD,
+        payload: {
+          person,
+          reason,
+          forgiven: false,
+          id: id()
+        }
+      });
+    },
+    [dispatch]
+  );
 
-  const toggleForgiveness = (id) => {
-    dispatch({
-      type: GRUDGE_FORGIVE,
-      payload: { id }
-    });
-  };
+  const toggleForgiveness = useCallback(
+    (id) => {
+      dispatch({
+        type: GRUDGE_FORGIVE,
+        payload: { id }
+      });
+    },
+    [dispatch]
+  );
 
   return (
     <div className="Application">
