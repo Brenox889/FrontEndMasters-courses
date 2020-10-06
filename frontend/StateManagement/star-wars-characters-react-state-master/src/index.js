c816cc68ba0c696d23232e4992583b484f128d6e
Brenox889 <brenomendes889@hotmail.com>
2020-10-05 23:19:24 -0300

    refactor: connecting to api

--- frontend/StateManagement/star-wars-characters-react-state-master/src/index.js
+++ frontend/StateManagement/star-wars-characters-react-state-master/src/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 
 import { BrowserRouter as Router } from 'react-router-dom';
@@ -10,7 +10,18 @@ import dummyData from './dummy-data';
 import './styles.scss';
 
 const Application = () => {
-  const [characters, setCharacters] = useState(dummyData);
+  const [characters, setCharacters] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch(endpoint + '/')
+      .then((response) => response.json())
+      .then((response) => {
+        setCharacters(response.characters);
+      })
+      .catch(console.error);
+  }, []);
 
   return (
     <div className="Application">
